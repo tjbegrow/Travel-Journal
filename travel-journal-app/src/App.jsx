9ad8e36dd888d80c3d44b3b40935415c68aab279
Tyler Begrow <tjbegrow@gmail.com>
2025-06-23 22:09:28 -0500

    Making cards

--- travel-journal-app/src/App.jsx
+++ travel-journal-app/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TokyoTower from './assets/TokyoTower.jpeg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
-      <h1>Hello World!</h1>
+      <header>
+        <h1>My Japanese Trip</h1>
+      </header>
+      <section className="location-card">
+        <div className="img-container">
+          <img className="card-img" src={TokyoTower}/>
+        </div>
+        <div className="description-container">
+          <h2>Tokyo Tower</h2>
+          <p id="date">2025年4月3日</p>
+          <p>p</p>
+        </div>
+      </section>
     </main>
   )
 }
